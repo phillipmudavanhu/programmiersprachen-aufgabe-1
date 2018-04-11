16ecfffa370356fb6daac6d1a60ed1459140a9cb
phillipmudavanhu <philcreativ@gmail.com>
2018-04-11 15:00:53 +0200

    Successful Aufgabe 1.8 Tests

--- source/tests.cpp
+++ source/tests.cpp
@@ -1,5 +1,21 @@
 #define CATCH_CONFIG_RUNNER
 #include <catch.hpp>
+#include <cmath>
+
+
+int gcd (int a, int b)
+{
+	return b == 0 ? a : gcd(b, a % b);
+}
+
+TEST_CASE ("describe_gcd", "[gcd]")
+{
+REQUIRE (gcd(2,4) == 2);
+REQUIRE (gcd(9,6) == 3);
+REQUIRE (gcd(3,7) == 1);
+}
+
+
 
 int main(int argc, char* argv[])
 {
