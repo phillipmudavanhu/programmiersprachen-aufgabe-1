9923fbf3a1a7abf0099560b81c8e5eb52a9ce722
phillipmudavanhu <philcreativ@gmail.com>
2018-04-11 16:43:14 +0200

    Aufgabe 1.11 added and Tests successful

--- source/tests.cpp
+++ source/tests.cpp
@@ -27,9 +27,9 @@ int checksum(int a)
 }
 
 
-int sumMultiples(int a, int b, int Count){ 
+int sumMultiples(int a, int b){ 
 		int sum = 0;
-		 for( int i = 1; i <= Count; ++i )
+		 for( int i = 1; i <= 1000; ++i )
 		 {			       
 			if (i%a==0 || i%b==0 )
 			   	{
@@ -39,6 +39,18 @@ int sumMultiples(int a, int b, int Count){
 
 	return sum;
 }
+
+
+//Aufgabe 1.11 Fraction Anteil---------------------------------------
+
+int fract(float frac)
+{
+    double integer, value = frac, number = modf(value, &integer);
+    return number;
+}
+
+
+
 
 //TESTS CASES---------------------- 
 
@@ -65,11 +77,21 @@ REQUIRE (checksum(-1) == -1);
 
 TEST_CASE ("describe_sumMultuples", "[sumMultiples]")
 {
-REQUIRE (sumMultiples(3,5,1000) == 234168);
-REQUIRE (sumMultiples(3,5,5) == 8);
-REQUIRE (sumMultiples(3,5,10) == 33);
-REQUIRE (sumMultiples(3,5,1) == 0);
+REQUIRE (sumMultiples (3,5) == 234168);
+REQUIRE (sumMultiples (1,5) == 500500);
+REQUIRE (sumMultiples (1,3) == 500500);
 }
+
+
+TEST_CASE("fract","[fract]")
+{
+	REQUIRE(7.009999 == Approx( 7.01 ).epsilon(0.001));
+	REQUIRE(0.009999 == Approx( 0.01 ).epsilon(0.001));
+	REQUIRE(1.9999 == Approx( 2.0 ).epsilon(0.001));
+}
+
+
+
 
 
 int main(int argc, char* argv[])
