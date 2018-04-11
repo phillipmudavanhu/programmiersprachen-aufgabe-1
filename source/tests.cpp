fa4101784387d7151ea8d1978a8887562789f20b
phillipmudavanhu <philcreativ@gmail.com>
2018-04-11 15:19:20 +0200

    Aufgabe 19 done successfully

--- source/tests.cpp
+++ source/tests.cpp
@@ -3,16 +3,51 @@
 #include <cmath>
 
 
+//Aufgabe 1.8 GCD Funktion Implementation ---------------------------
+
+
 int gcd (int a, int b)
 {
 	return b == 0 ? a : gcd(b, a % b);
 }
+
+
+//Aufgabe 1.9 CheckSum  Funktion Implementation---------------------- 
+
+int checksum(int a)
+{ 
+	int sum = 0;
+
+    while (a != 0)
+    {
+        sum += a % 10;
+        a /= 10;
+    }
+    return sum;
+}
+
+
+
+
+
+
+//TESTS CASES---------------------- 
+
 
 TEST_CASE ("describe_gcd", "[gcd]")
 {
 REQUIRE (gcd(2,4) == 2);
 REQUIRE (gcd(9,6) == 3);
 REQUIRE (gcd(3,7) == 1);
+}
+
+
+TEST_CASE ("describe_checksum", "[checksum]")
+{
+REQUIRE (checksum(112601) == 11);
+REQUIRE (checksum(000000) == 0);
+REQUIRE (checksum(111111) == 6);
+
 }
 
 
